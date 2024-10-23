c5d50d1aef8207d660df7b66f4f06e7f6161bb17
CN-alexchenod <alex.chen.od@cn.mcd.com>
2024-10-23 14:47:57 +0800

    spider bilibili fix

--- 04-常用工程类库/爬虫/04-ikun_bilibili.py
+++ 04-常用工程类库/爬虫/04-ikun_bilibili.py
@@ -2,6 +2,7 @@
 
 # 最新版的selenium(4.x.x)已经不支持PhantomJS。如要用PhantomJS，可用旧版本selenium。如pip install selenium==3.8.0。
 from selenium import webdriver
+from selenium.webdriver.edge.options import Options
 from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
@@ -9,8 +10,11 @@ from selenium.webdriver.support import expected_conditions as EC
 from bs4 import BeautifulSoup
 import xlwt
 
-# browser = webdriver.PhantomJS()
-browser = webdriver.Chrome()
+# 设置 Edge 无头模式选项
+options = Options()
+options.use_chromium = True
+# options.add_argument("--headless")  # 启用无头模式
+browser = webdriver.Edge(options)
 WAIT = WebDriverWait(browser, 10)
 browser.set_window_size(1400, 900)
 
@@ -19,10 +23,9 @@ book = xlwt.Workbook(encoding='utf-8', style_compression=0)
 sheet = book.add_sheet('蔡徐坤篮球', cell_overwrite_ok=True)
 sheet.write(0, 0, '名称')
 sheet.write(0, 1, '地址')
-sheet.write(0, 2, '描述')
-sheet.write(0, 3, '观看次数')
-sheet.write(0, 4, '弹幕数')
-sheet.write(0, 5, '发布时间')
+sheet.write(0, 2, '观看次数')
+sheet.write(0, 3, '弹幕数')
+sheet.write(0, 4, '发布时间')
 
 n = 1
 
@@ -36,9 +39,11 @@ def search():
         # index = WAIT.until(EC.element_to_be_clickable((By.CSS_SELECTOR, "#primary_menu > ul > li.home > a")))
         # index.click()
 
-        input = WAIT.until(EC.presence_of_element_located((By.CSS_SELECTOR, "#nav_searchform > input")))
-        submit = WAIT.until(EC.element_to_be_clickable(
-            (By.XPATH, '/html/body/div[2]/div/div[1]/div[1]/div/div[2]/div/form/div/button')))
+        # input = WAIT.until(EC.presence_of_element_located((By.CSS_SELECTOR, "#nav-searchform > div > input")))
+        input = WAIT.until(EC.presence_of_element_located((By.CSS_SELECTOR, ".nav-search-input")))
+        # submit = WAIT.until(EC.element_to_be_clickable(
+        #     (By.XPATH, '/html/body/div[2]/div/div[1]/div[1]/div/div[2]/div/form/div/button')))
+        submit = WAIT.until(EC.element_to_be_clickable((By.CSS_SELECTOR, '.nav-search-btn')))
 
         input.send_keys('蔡徐坤 篮球')
         submit.click()
@@ -47,11 +52,10 @@ def search():
         print('跳转到新窗口')
         all_h = browser.window_handles
         browser.switch_to.window(all_h[1])
-        get_source()
-
-        total = WAIT.until(EC.presence_of_element_located((By.CSS_SELECTOR,
-                                                           "#all-list > div.flow-loader > div.page-wrap > div > ul > li.page-item.last > button")))
-        return int(total.text)
+        soup = get_source()
+        save_to_excel(soup)
+        total_page = soup.find(class_='vui_pagenation--btns').find_all(class_='vui_pagenation--btn-num')[-1].text
+        return int(total_page)
     except TimeoutException:
         return search()
 
@@ -59,28 +63,27 @@ def search():
 def next_page(page_num):
     try:
         print('获取下一页数据')
-        next_btn = WAIT.until(EC.element_to_be_clickable((By.CSS_SELECTOR,
-                                                          '#all-list > div.flow-loader > div.page-wrap > div > ul > li.page-item.next > button')))
+        page_btns = WAIT.until(EC.element_to_be_clickable((By.CSS_SELECTOR, '.vui_pagenation--btns')))
+        next_btn = page_btns.find_elements(By.TAG_NAME, 'button')[-1]
         next_btn.click()
-        WAIT.until(EC.text_to_be_present_in_element((By.CSS_SELECTOR,
-                                                     '#all-list > div.flow-loader > div.page-wrap > div > ul > li.page-item.active > button'),
-                                                    str(page_num)))
-        get_source()
+        WAIT.until(EC.text_to_be_present_in_element((By.CSS_SELECTOR, '.vui_button--active-blue'), str(page_num)))
+        soup = get_source()
+        save_to_excel(soup)
     except TimeoutException:
         browser.refresh()
         return next_page(page_num)
 
 
 def save_to_excel(soup):
-    list = soup.find(class_='video-list clearfix').find_all(class_='video-item matrix')
+    list = soup.find(class_='video-list row').find_all(class_='bili-video-card__wrap __scale-wrap')
 
     for item in list:
-        item_title = item.find('a').get('title')
+        item_title = item.find('h3').get('title')
         item_link = item.find('a').get('href')
-        item_dec = item.find(class_='des hide').text
-        item_view = item.find(class_='so-icon watch-num').text
-        item_biubiu = item.find(class_='so-icon hide').text
-        item_date = item.find(class_='so-icon time').text
+        stats = item.findAll(class_='bili-video-card__stats--item')
+        item_view = stats[0].find('span').text
+        item_biubiu = stats[1].find('span').text
+        item_date = item.find(class_='bili-video-card__info--date').text
 
         print('爬取：' + item_title)
 
@@ -88,23 +91,23 @@ def save_to_excel(soup):
 
         sheet.write(n, 0, item_title)
         sheet.write(n, 1, item_link)
-        sheet.write(n, 2, item_dec)
-        sheet.write(n, 3, item_view)
-        sheet.write(n, 4, item_biubiu)
-        sheet.write(n, 5, item_date)
+        sheet.write(n, 2, item_view)
+        sheet.write(n, 3, item_biubiu)
+        sheet.write(n, 4, item_date[3:])
 
         n = n + 1
 
 
 def get_source():
-    WAIT.until(EC.presence_of_element_located(
-        (By.CSS_SELECTOR, '#all-list > div.flow-loader > div.filter-wrap')))
+    # WAIT.until(EC.presence_of_element_located(
+    #     (By.CSS_SELECTOR, '#all-list > div.flow-loader > div.filter-wrap')))
+
+    WAIT.until(EC.presence_of_element_located((By.CSS_SELECTOR, '.video-list')))
 
     html = browser.page_source
     soup = BeautifulSoup(html, 'lxml')
     print('到这')
-
-    save_to_excel(soup)
+    return soup
 
 
 def main():
@@ -121,4 +124,4 @@ def main():
 
 if __name__ == '__main__':
     main()
-    book.save('蔡徐坤篮球.xlsx')
+    book.save('蔡徐坤篮球.xls')
