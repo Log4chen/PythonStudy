0566e568331a7a4cd027d4e094fb204dc9efb3df
CN-alexchenod <alex.chen.od@cn.mcd.com>
2024-11-18 15:38:09 +0800

    OCR&echarts

---
+++ 03-常用工具类库/08-echarts.py
@@ -0,0 +1,44 @@
+from pyecharts import options as opts
+from pyecharts.charts import WordCloud, Bar
+from pyecharts.render import make_snapshot
+from snapshot_selenium import snapshot
+
+bar = Bar()
+bar.add_xaxis(["衬衫", "羊毛衫", "雪纺衫", "裤子", "高跟鞋", "袜子"])
+bar.add_yaxis("商家A", [5, 20, 36, 10, 75, 90])
+# render 会生成本地 HTML 文件，默认会在当前目录生成 render.html 文件
+# 也可以传入路径参数，如 bar.render("bar.html")
+bar.render("bar.html")
+
+words = [
+    ("Sam S Club", 10000),
+    ("Macys", 6181),
+    ("Amy Schumer", 4386),
+    ("Jurassic World", 4055),
+    ("Charter Communications", 2467),
+    ("Chick Fil A", 2244),
+    ("Planet Fitness", 1868),
+    ("Pitch Perfect", 1484),
+    ("Express", 1112),
+    ("Home", 865),
+    ("Johnny Depp", 847),
+    ("Lena Dunham", 582),
+    ("Lewis Hamilton", 555),
+    ("KXAN", 550),
+    ("Mary Ellen Mark", 462),
+    ("Farrah Abraham", 366),
+    ("Rita Ora", 360),
+    ("Serena Williams", 282),
+    ("NCAA baseball tournament", 273),
+    ("Point Break", 265),
+]
+
+
+wc = WordCloud()
+wc.add("", words, word_size_range=[20, 100])
+wc.set_global_opts(title_opts=opts.TitleOpts(title="词云"))
+
+html_file_path = wc.render("WordCloud.html")
+
+# 需要安装 snapshot_selenium
+make_snapshot(snapshot, html_file_path, "WordCloud.png")
